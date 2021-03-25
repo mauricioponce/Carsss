47d44eb9d09ecc4e670a8038e512e5909f3c0e5b
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2021-03-25 19:17:27 -0300

    proyecto limpiod desde template

---
+++ app/src/main/java/cl/dal/cars/MainActivity.kt
@@ -0,0 +1,23 @@
+package cl.dal.cars
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * [X] control de versiones
+ * [ ]
+ * [ ]
+ * [ ]
+ * [ ]
+ * [ ]
+ * [ ]
+ * [ ]
+ * [ ]
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
